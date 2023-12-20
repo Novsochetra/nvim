552ad20bf5e1d9e1d73fd627b46fd3db427f7539
Sochetra NOV <sochetra12.nov@gmail.com>
2023-12-20 10:21:30 +0700

    chore(harpoon): remove map key ctrl + n and ctrl + p

--- plugin-config/harpoon.lua
+++ plugin-config/harpoon.lua
@@ -7,6 +7,4 @@ harpoon:setup()
 vim.keymap.set("n", "<F3>", function() harpoon:list():append() end)
 vim.keymap.set("n", "<F4>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
-vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<F5>", ':source ~/.config/nvim/init.vim<CR>')
