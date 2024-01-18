0f944b007e69b30a3248f090082e6004c0e86ccc
Sochetra NOV <sochetra12.nov@gmail.com>
2024-01-18 08:53:59 +0700

    fix: harpoon doesn't save file while toggle menu

--- plugin-config/harpoon.lua
+++ plugin-config/harpoon.lua
@@ -1,7 +1,15 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({
+  settings = {
+    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+    save_on_toggle = true,
+
+    -- saves the harpoon file upon every change. disabling is unrecommended.
+    save_on_change = true
+  }
+})
 -- REQUIRED
 
 vim.keymap.set("n", "<F3>", function() harpoon:list():append() end)
