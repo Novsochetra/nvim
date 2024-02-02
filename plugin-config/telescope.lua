79e73431003479f198622d4ae8626a6dcb76b5f7
Sochetra NOV <sochetra12.nov@gmail.com>
2024-02-02 09:17:37 +0700

    chore: update & adding key map to telescope

--- plugin-config/telescope.lua
+++ plugin-config/telescope.lua
@@ -12,7 +12,19 @@ telescope.setup {
       },
     },
   },
+
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
 }
+
+pcall(require('telescope').load_extension, 'fzf')
 
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -20,5 +32,9 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = 'Search in current file' })
-vim.keymap.set('n', '<leader>t', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>tt', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = 'Search word in files in a working project directory' })
+
+vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope (which keymap is no our neovim)' })
