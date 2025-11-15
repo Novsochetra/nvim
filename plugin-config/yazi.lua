2b07c4c53ce1e390fcdb685254426ac169ff5b5b
SochetraNOV <sochetra12.nov@gmail.com>
2025-11-15 21:43:27 +0700

    chore(yazi): improve when using cd to auto using current dir for yazi

--- plugin-config/yazi.lua
+++ plugin-config/yazi.lua
@@ -1,30 +1,31 @@
-vim.keymap.set("n", "<leader>n", function()
-  require("yazi").yazi({
-    open_for_directories = true,          -- allow folder navigation
-    change_neovim_cwd_on_close = true,
-    hooks = {
-      yazi_closed_successfully = function(chosen_file, config, state)
-        -- INFO: when we selected the file we will need to change neovim working dir
-        -- otherwise plugin like telescope.find_files will search the default folder
-        if chosen_file then
-          local path = vim.fn.fnamemodify(chosen_file, ":p") -- full path
+-- vim.keymap.set("n", "<leader>n", function()
+--   require("yazi").yazi({
+--     open_for_directories = true,          -- allow folder navigation
+--     change_neovim_cwd_on_close = true,
+--     hooks = {
+--       yazi_closed_successfully = function(chosen_file, config, state)
+--         -- INFO: when we selected the file we will need to change neovim working dir
+--         -- otherwise plugin like telescope.find_files will search the default folder
+--         if chosen_file then
+--           local path = vim.fn.fnamemodify(chosen_file, ":p") -- full path
+--
+--           -- check if it is a directory
+--           if vim.fn.isdirectory(path) == 1 then
+--             vim.cmd("cd " .. path)
+--             vim.notify("Neovim CWD changed to directory: " .. path)
+--           else
+--             -- it's a file, set cwd to parent folder
+--             local dir = vim.fn.fnamemodify(path, ":h")
+--             vim.cmd("cd " .. dir)
+--             vim.notify("Neovim CWD changed to parent folder: " .. dir)
+--           end
+--         end
+--       end
+--     }
+--   })
+-- end)
 
-          -- check if it is a directory
-          if vim.fn.isdirectory(path) == 1 then
-            vim.cmd("cd " .. path)
-            vim.notify("Neovim CWD changed to directory: " .. path)
-          else
-            -- it's a file, set cwd to parent folder
-            local dir = vim.fn.fnamemodify(path, ":h")
-            vim.cmd("cd " .. dir)
-            vim.notify("Neovim CWD changed to parent folder: " .. dir)
-          end
-        end
-      end
-    }
-  })
-end)
-
+vim.keymap.set("n", "<leader>n", ":Yazi cwd<CR>", { desc = "Open Yazi for current dir" })
 vim.keymap.set("n", "<leader>vc", function()
   vim.cmd("cd ~/.config/nvim")
 end, { desc = "Open Nvim config directory with default explorer" })
